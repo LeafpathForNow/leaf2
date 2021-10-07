1ed626f6cbb62e96d464f37d662aa1c9f7e12931
LeafpathForNow <92127089+LeafpathForNow@users.noreply.github.com>
2021-10-07 18:06:19 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,20 @@
 
 # For Heroku, make sure your .py includes the following.
 import os # import the OS details, including our hidden bot token
-token = os.environ.get('DISCORD_BOT_TOKEN') # fetch the token from Heroku's "OS" running the bot. make sure the name matches the one you've used on Heroku
+token = os.environ.get('leaf2') # fetch the token from Heroku's "OS" running the bot. make sure the name matches the one you've used on Heroku
+async def my_background_task():
+    await client.wait_until_ready() # ensures cache is loaded
+    counter = 0
+    channel = client.get_channel(id=895809139006132277) # replace with target channel id
+    while not client.is_closed():
+        counter += 1
+        await channel.send(counter)
+        await asyncio.sleep(300) # or 300 if you wish for it to be 5 minutes
+
+@client.event
+async def on_ready():
+    print('s.fish')
+    client.loop.create_task(my_background_task()) # best to put it in here
 
 # Include this at the end of your code. Instead of bot, you may have "discord.Client()" "commands.Bot()" etc, or whatever you have defined these.
-bot.run(token) # make sure your token variable matches the token defined above
+client.run(token) # make sure your token variable matches the token defined above
